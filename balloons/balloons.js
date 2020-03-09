0b8e6ab6e673257e2242069e1e98a621c84aae94
rachelvo <rach0727@stud.kea.dk>
2020-03-09 15:49:56 +0100

    added unfinished balloons

--- balloons/balloons.js
+++ balloons/balloons.js
@@ -4,7 +4,14 @@ window.addEventListener("DOMContentLoaded", start);
 
 function start() {
   // load the SVG into #balloon
+  loadBalloon();
   // then call createBalloons
+  createBalloons();
+}
+async function loadBalloon() {
+  const response = await fetch("Yellow_toy_balloon.svg");
+  const svg = await response.text();
+  document.querySelector("#balloon").innerHTML = svg;
 }
 
 function createBalloons() {
@@ -15,7 +22,17 @@ function createBalloons() {
 
 function createBalloon() {
   // create a single balloon, using the #yellow_balloon
+  let balloon = document.createElementNS("http://www.w3.org/2000/svg", "use");
+  balloon.setAttribute("href", "#yellow_balloon");
+
   // give it a random x and y
+  let x = Math.floor(Math.random() * 5000);
+  let y = Math.floor(Math.random() * 10000);
+  balloon.setAttribute("x", x);
+  balloon.setAttribute("y", y);
+
   // maybe also add a bit of animation ...
+  balloon.classList.add("rise");
   // before adding it to the #balloons
+  document.querySelector("#balloons").append(balloon);
 }
