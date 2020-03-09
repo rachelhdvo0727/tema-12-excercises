51f2773ad0e23513cd96a10073640039c431e749
rachelvo <rach0727@stud.kea.dk>
2020-03-09 08:57:35 +0100

    added balloons file

---
+++ balloons/balloons.js
@@ -0,0 +1,21 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  // load the SVG into #balloon
+  // then call createBalloons
+}
+
+function createBalloons() {
+  for (let i = 0; i < 40; i++) {
+    createBalloon();
+  }
+}
+
+function createBalloon() {
+  // create a single balloon, using the #yellow_balloon
+  // give it a random x and y
+  // maybe also add a bit of animation ...
+  // before adding it to the #balloons
+}
